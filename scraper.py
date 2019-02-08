95049c56a46c770e8faa4e5fb21781d60fc48f6a
Josh Berenguel <joshberenguel@gmail.com>
2019-02-07 22:50:25 -0800

    Add ScanForContests

--- scraper.py
+++ scraper.py
@@ -15,6 +15,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 # ---Import needed modules from Beautiful Soup
 from bs4 import BeautifulSoup
+from requests_oauthlib import OAuth1
 
 # Load our configuration from the JSON file.
 with open('config.json') as data_file:
@@ -27,27 +28,30 @@ access_token_key = data["access-token-key"]
 access_token_secret = data["access-token-secret"]
 search_queries = data["search-queries"]
 retweet_update_time = data["retweet-update-time"]
+scan_update_time = data["scan-update-time"]
 rate_limit_update_time = data["rate-limit-update-time"]
 min_ratelimit = data["min-ratelimit"]
 min_ratelimit_retweet = data["min-ratelimit-retweet"]
-min_ratelimit_search = data["min-ratelimit-search"]
 username_or_email = data["username-or-email"]
 password = data["password"]
 bannedusers = data["banned"]
+# End loaded data from config file
 
 base_url = 'https://twitter.com/'
 query_string = 'search?q='
 login_path = 'login'
-
+loginError = "The username and password you entered did not match Twitter's records.  " + \
+    "Please double-check and try again."
 # ---selenium command
 browser = webdriver.Chrome()
+auth = OAuth1(consumer_key, consumer_secret, access_token_key, access_token_secret)
 
 # --- TwitterAPI OAuth command
 api = TwitterAPI(consumer_key, consumer_secret, access_token_key,
                  access_token_secret)
 post_list = list()
 ignore_list = list()
-
+ratelimit = [999, 999, 100]  # [limit,remaining,percent]
 count = 1
 
 if os.path.isfile('ignorelist'):
@@ -57,60 +61,27 @@ if os.path.isfile('ignorelist'):
     f.close()
 
 
-# Print and log the tetxt
+# --- Print and log the tetxt
 def LogAndPrint(text):
     tmp = str(text)
     tmp = text.replace("\n", "")
     print(tmp)
-    open(file='log', mode='a', encoding='utf-8')
-    f_log = open(
-        'log',
-        'a',
-    )
+    f_log = open(file='log', mode='a', encoding='utf-8')
     f_log.write(tmp + "\n")
     f_log.close()
+# end LogAndPrint(text)
 
 
-def CheckRateLimit():
-    c = threading.Timer(rate_limit_update_time, CheckRateLimit)
-    c.daemon = True
-    c.start()
-
-    global ratelimit
-    global ratelimit_search
-
-    if ratelimit[2] < min_ratelimit:
-        print("Ratelimit too low -> Cooldown (" + str(ratelimit[2]) + "%)")
-        time.sleep(200)
-
-        r = api.request('application/rate_limit_status').json()
-
-        if 'resources' in r:
-            for res_family in r['resources']:
-                for res in r['resources'][res_family]:
-                    limit = r['resources'][res_family][res]['limit']
-                    remaining = r['resources'][res_family][res]['remaining']
-                    percent = float(remaining) / float(limit) * 100
-
-                    if res == "/application/rate_limit_status":
-                        ratelimit = [limit, remaining, percent]
-
-                    # print(res_family + " -> " + res + ": " + str(percent))
-                    if percent < 5.0:
-                        LogAndPrint(res_family + " -> " + res + ": " +
-                                    str(percent) +
-                                    "  !!! <5% Emergency exit !!!")
-                        sys.exit(res_family + " -> " + res + ": " +
-                                 str(percent) + "  !!! <5% Emergency exit !!!")
-                    elif percent < 30.0:
-                        LogAndPrint(res_family + " -> " + res + ": " +
-                                    str(percent) + "  !!! <30% alert !!!")
-                    elif percent < 70.0:
-                        print(res_family + " -> " + res + ": " + str(percent))
+def CheckError(r):
+    r = r.json()
+    if 'errors' in r:
+        LogAndPrint("Error message: " + r['errors'][0]
+                    ['message'] + " Code: " + str(r['errors'][0]['code']))
+# end CheckError(r)
 
 
 # --- Login to Twitter using selenium
-def login():
+def Login():
     url = (base_url + login_path)
     browser.implicitly_wait(30)
     browser.get(url)
@@ -127,8 +98,13 @@ def login():
         "(//input[@name='session[password]'])[2]").send_keys(password)
     browser.find_element_by_xpath("//button[@type='submit']").click()
     WebDriverWait(browser, 10)
+    new_page = browser.find_element_by_tag_name('html')
+    if('https://twitter.com/login/error?' in new_page.id):
+        LogAndPrint(loginError)
+        sys.exit(loginError)
 
 
+# --- check if user is a bot hunter
 def is_user_bot_hunter(username):
     username = username.replace("0", "o").lower()
     for i in bannedusers:
@@ -140,7 +116,12 @@ def is_user_bot_hunter(username):
 
 # --- Run all search queries using Beautiful Soup
 def ScanForContests():
+    t = threading.Timer(scan_update_time, ScanForContests)
+    t.daemon = True
+    t.start()
+
     global count
+    global ratelimit_search
 
     for search_query in search_queries:
 
@@ -171,24 +152,23 @@ def ScanForContests():
                 user_id = str(details['data-user-id'])
                 tweet_id = str(details['data-tweet-id'])
                 data_name = details['data-name']
+                print('USERNAMEUSERNAME:' + user_id)
                 if user_id not in ignore_list and\
                         tweet_id not in ignore_list and\
                         is_user_bot_hunter(data_name) is False:
                     div = details.find('div', {'class': 'content'})
-                    tweet_text = div.find('div', {
+                    tweet_text = str(div.find('div', {
                         'class': 'js-tweet-text-container'
-                    }).text.strip().encode("utf-8")
-                    temp = details['data-screen-name'].encode("utf-8")
-                    user_name = str(temp)
+                    }).text.strip())
+                    user_name = str(details['data-screen-name'])
                     print('*********************')
-                    # if "bot" not in str(details['data-name'].
-                    #                   encode("utf-8").lower()):
-
-                    print('{}\nUser ID: {}\nTweet ID: {}\nTweet: {}\n'.format(
-                        user_name, user_id, tweet_id, tweet_text))
-                    # if
-
-            # TODO: add to listb
+                    # .format(user_name, user_id, tweet_id, tweet_text))
+                    print(f'{user_name}\nUser ID: {user_id}')
+                    print(f'Tweet ID: {tweet_id}\nTweet: {tweet_text}')
+                    f_ign = open('ignorelist', 'a')
+                    ignore_list.append(tweet_id)
+                    f_ign.close()
+                    api.request('statuses/retweet/:' + tweet_id)
                 count += 1
             except KeyError as e:
                 print('Error extracting ' + str(e) + ' data from class:')
@@ -198,8 +178,11 @@ def ScanForContests():
         # end for details in soup.find_all(...)
         print('Total found: ' + str(count))
     # end for search_query in search_queries
-
-
 # end ScanForContests
-login()
-ScanForContests()
+
+
+Login()
+# CheckRateLimit()
+while (True):
+    ScanForContests()
+    time.sleep(3600)
